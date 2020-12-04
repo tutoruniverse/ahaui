d88852aa8d2ec34f5a4582664875cfd2fe60996d
nam-gotit <lenkc98@gmail.com>
2020-12-04 10:28:39 +0700

    feat: add infra

---
+++ infra/docs.deploy.sh
@@ -0,0 +1,8 @@
+if [ $CODEBUILD_BUILD_SUCCEEDING -eq 1 ]
+then
+  REACT_APP_ENV=`echo ${ENV} | tr [a-z] [A-Z]`
+  echo "s3://$S3"
+  aws s3 sync --delete --cache-control max-age=2592000,public www/public/ s3://$S3
+  aws s3 cp s3://$S3/index.html s3://$S3/index.html --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-type text/html
+  aws cloudfront create-invalidation --distribution-id $CLOUDFRONT --paths "/*"
+fi
