dd9b917ed0eee1f04d68c9b0e29c6d9890090292
Kyle TV <tinhvqbk@gmail.com>
2021-01-15 16:38:57 +0700

    feat: added seerch Comp for Icon

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -185,7 +185,7 @@ const CustomTheme = () => {
               showGutter={true}
               highlightActiveLine={true}
               wrapEnabled={true}
-              width={700}
+              width={'700px'}
               setOptions={{
                 enableBasicAutocompletion: true,
                 enableLiveAutocompletion: true,
