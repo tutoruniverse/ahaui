f04bf07b2be1574bcdfdc38bc8d81c2b00b33a72
Thang Tran <uendno@gmail.com>
2021-03-25 17:42:52 +0700

    fix: Proxy

--- infra/edge/src/proxyIndex.js
+++ infra/edge/src/proxyIndex.js
@@ -1,18 +1,31 @@
 const path = require('path');
 
 exports.handler = (event, context, callback) => {
-    const request = event.Records[0].cf.request;
-    // var uri = request.uri;
-    // const parsed_path = path.parse(uri);
-  
-    // // Extract version part
-    // const version = uri.match(/^\/[0-9]+\.[0-9]+\.[0-9]+/g);
-    
-    // if (version && parsed_path.ext === ''){
-    //     uri = version + '/index.html'
-    // }
+  const request = event.Records[0].cf.request;
+  var uri = request.uri;
 
-    // request.uri = uri
-   
-    callback(null, request);
+  console.log(uri)
+
+  // Extract version part
+  const version = uri.match(/^\/[0-9]+\.[0-9]+\.[0-9]+/g);
+  const formattedUri = uri.replace(version, '');
+
+  if (version) {
+    const parsed_path = path.parse(formattedUri);
+    if (parsed_path.ext === '') {
+      if (uri.endsWith('/')) {
+        uri = uri + 'index.html'
+      } else {
+        uri = uri + '/index.html'
+      }
+    }
+  } else {
+    uri = '/index.html'
   }
+
+  request.uri = uri
+
+  console.log('Redirect to', uri)
+
+  callback(null, request);
+}
