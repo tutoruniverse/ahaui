dbd022353b2c8a5bd1adbdcd7a2c9c49ddb18d5b
Thang Tran <uendno@gmail.com>
2022-05-04 16:57:38 +0700

    chore: Fix rendering index.html

--- renderRedirectIndex.js
+++ renderRedirectIndex.js
@@ -4,4 +4,9 @@ const ahaReactConfig = require('./config');
 
 const templateFile = fs.readFileSync('redirect.ejs', 'utf-8');
 const html = ejs.render(templateFile, { version: ahaReactConfig.version });
+try {
+  fs.mkdirSync('gh-pages-root');
+} catch(e) {
+  //ignore
+}
 fs.writeFileSync('gh-pages-root/index.html', html);
