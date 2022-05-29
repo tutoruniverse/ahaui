40d2d28cbc822e9dbaa16a6eeab3c427cad8ea08
tinhvqbk <tinhvqbk@gmail.com>
2022-05-29 18:07:26 +0700

    fix: Overlay import

--- apps/docs/src/examples/Overlay/tooltipWhite.js
+++ apps/docs/src/examples/Overlay/tooltipWhite.js
@@ -1,21 +1,19 @@
-['top', 'right', 'bottom', 'left'].map(placement => (
-  <Overlay.Trigger
-    key={placement}
-    placement={placement}
-    overlay={props => (
-      <Tooltip id="tooltip-{placement}" variant="white" {...props}>
-        <b>Tooltips</b>
-       &nbsp;
-on
-       &nbsp;
-        <strong>{placement}</strong>
-.
-      </Tooltip>
-    )}
-  >
-    <Button variant="secondary" className="u-marginHorizontalExtraSmall">
-      Tooltip on
-      {placement}
-    </Button>
-  </Overlay.Trigger>
-));
+() =>
+  ["top", "right", "bottom", "left"].map((placement) => (
+    <Overlay.Trigger
+      key={placement}
+      placement={placement}
+      overlay={(props) => (
+        <Tooltip id="tooltip-{placement}" variant="white" {...props}>
+          <b>Tooltips</b>
+          &nbsp; on &nbsp;
+          <strong>{placement}</strong>.
+        </Tooltip>
+      )}
+    >
+      <Button variant="secondary" className="u-marginHorizontalExtraSmall">
+        Tooltip on
+        {placement}
+      </Button>
+    </Overlay.Trigger>
+  ));
