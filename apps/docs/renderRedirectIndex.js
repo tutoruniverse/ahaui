e1a0588da5e92d39eca7fc59927aa6637a7aab41
tinhvqbk <tinhvqbk@gmail.com>
2022-10-23 02:12:35 +0700

    docs: Update redirect docs link
    (cherry picked from commit ffe7b3140d52dc6d9c0f6356e597fe5d5c751d52)

--- apps/docs/renderRedirectIndex.js
+++ apps/docs/renderRedirectIndex.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const ahaReactConfig = require('./config');
 
 const templateFile = fs.readFileSync('redirect.ejs', 'utf-8');
-const html = ejs.render(templateFile, { version: ahaReactConfig.version.split('.')[0] });
+const html = ejs.render(templateFile, { version: ahaReactConfig.version.split('.')[0], host: process.env.GATSBY_APP_ENV === 'dev' ? 'https://ahaui-insiders.github.io/' : 'https://ahaui.github.io/' });
 try {
   fs.mkdirSync('gh-pages-root');
 } catch(e) {
