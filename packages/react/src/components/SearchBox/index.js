893d05209a29d76a397f035d2c3182c5b40dc711
tinhvqbk <tinhvqbk@gmail.com>
2022-10-22 22:12:41 +0700

    react: Add searchButtonProps on SearchBox

--- packages/react/src/components/SearchBox/index.js
+++ packages/react/src/components/SearchBox/index.js
@@ -5,7 +5,6 @@ import Form from 'components/Form';
 import Icon from 'components/Icon';
 import Button from 'components/Button';
 
-
 const propTypes = {
   /** Callback fired when the Search Button is clicking */
   onClickButton: PropTypes.func,
@@ -19,41 +18,55 @@ const propTypes = {
   sizeControl: PropTypes.string,
   /** Custom text button */
   buttonText: PropTypes.string,
+  /**
+   * A set of SearchButton props
+   */
+  searchButtonProps: PropTypes.object,
 };
 
 const defaultProps = {
   buttonIcon: 'search',
+  searchButtonProps: {},
 };
 
-const SearchBox = React.forwardRef(({ className, sizeControl, onClickButton, buttonIcon, buttonText, ...props }, ref) => (
-
-  <Form.Group
-    sizeControl={sizeControl}
-    className={classNames(
-      'SearchBox',
-      className && className
-    )}
-  >
-    <Form.InputGroup>
-      <Form.Input
-        ref={ref}
-        {...props}
-        type="search"
-      />
-      <Form.InputGroup.Append>
-        <Button size={sizeControl} variant="secondary" onClick={onClickButton}>
-          {buttonText && (
-          <Button.Label>{buttonText}</Button.Label>
-          )}
-          {buttonIcon && (
-          <Button.Icon><Icon name={buttonIcon} /></Button.Icon>
-          )}
-        </Button>
-      </Form.InputGroup.Append>
-    </Form.InputGroup>
-  </Form.Group>
-
-));
+const SearchBox = React.forwardRef(
+  (
+    {
+      className,
+      sizeControl,
+      onClickButton,
+      buttonIcon,
+      buttonText,
+      searchButtonProps,
+      ...props
+    },
+    ref,
+  ) => (
+    <Form.Group
+      sizeControl={sizeControl}
+      className={classNames('SearchBox', className && className)}
+    >
+      <Form.InputGroup>
+        <Form.Input ref={ref} {...props} type="search" />
+        <Form.InputGroup.Append>
+          <Button
+            size={sizeControl || searchButtonProps?.size}
+            variant="secondary"
+            onClick={onClickButton || searchButtonProps?.onClick}
+            {...searchButtonProps}
+          >
+            {buttonText && <Button.Label>{buttonText}</Button.Label>}
+            {buttonIcon && (
+              <Button.Icon>
+                <Icon name={buttonIcon} />
+              </Button.Icon>
+            )}
+          </Button>
+        </Form.InputGroup.Append>
+      </Form.InputGroup>
+    </Form.Group>
+  ),
+);
 
 SearchBox.displayName = 'SearchBox';
 SearchBox.defaultProps = defaultProps;
