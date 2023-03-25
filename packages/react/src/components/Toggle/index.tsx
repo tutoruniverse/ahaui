f8c5f9011db064eeabdc927ed869b1aa48951601
Kriss Pham <krisspham@gotitapp.co>
2023-03-25 21:35:10 +0700

    react: Add typing for Toggle

--- packages/react/src/components/Toggle/index.tsx
+++ packages/react/src/components/Toggle/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import { PrefixProps, RefForwardingComponent } from 'interfaces/helpers';
+
+export enum ToggleValue {
+  ON = 'On',
+  OFF = 'Off',
+}
 
 const propTypes = {
   /** Set the visual state of the Toggle to active */
@@ -21,50 +27,68 @@ const defaultProps = {
   checked: false,
   disabled: false,
   nonLabel: false,
-  textLabelOn: 'On',
-  textLabelOff: 'Off',
+  textLabelOn: ToggleValue.ON,
+  textLabelOff: ToggleValue.OFF,
   ariaLabel: 'Toggle',
 };
 
-const Toggle = React.forwardRef(({ className, checked, disabled, nonLabel, textLabelOn, textLabelOff, ariaLabel, as: Component = 'button', ...props }, ref) => (
-  <div
-    className={classNames(
-      'u-flexInline u-alignItemsCenter',
-      className && className
-    )}
-    {...props}
-    ref={ref}
-  >
-    <Component
-      className={classNames(
-        'Toggle',
-        'u-positionRelative u-backgroundSemiLight u-textDark u-paddingNone u-borderNone u-cursorPointer',
-        checked && 'checked u-backgroundPrimary',
-        disabled && 'is-disabled u-cursorNotAllow u-pointerEventsNone',
-      )}
-      aria-label={ariaLabel}
-      // data-label-on={!nonLabel ? textLabelOn : undefined}
-      // data-label-off={!nonLabel ? textLabelOff : undefined}
-      disabled={(Component === 'button') ? disabled : undefined}
+interface ToggleProps extends PrefixProps, React.HTMLAttributes<HTMLDivElement> {
+  /** Set the visual state of the Toggle to active */
+  checked: boolean;
+  /** Disables the disabled, preventing mouse events, even if the underlying component is an `<a>` element */
+  disabled: boolean;
+  /** Disable the label */
+  nonLabel: boolean;
+  /** Custom label for the on-state */
+  textLabelOn: string;
+  /** Custom label for the off-state */
+  textLabelOff: string;
+  /** Custom aria label for accessibility */
+  ariaLabel: string;
+}
+
+export const Toggle: RefForwardingComponent<'div', ToggleProps> = React.forwardRef<any, ToggleProps>(
+  (
+    {
+      className,
+      checked,
+      disabled,
+      nonLabel,
+      textLabelOn,
+      textLabelOff,
+      ariaLabel,
+      as: Component = 'button',
+      ...props
+    },
+    ref
+  ) => (
+    <div
+      className={classNames('u-flexInline u-alignItemsCenter', className && className)}
+      {...props}
+      ref={ref}
     >
-      <div className={classNames(
-        'Toggle-handle',
-        'u-positionAbsolute u-backgroundWhite'
-      )}
-      />
-    </Component>
-    {!nonLabel && (
-      <div className={classNames(
-        'u-marginLeftExtraSmall',
-        disabled && 'u-textLight'
-      )}
+      <Component
+        className={classNames(
+          'Toggle',
+          'u-positionRelative u-backgroundSemiLight u-textDark u-paddingNone u-borderNone u-cursorPointer',
+          checked && 'checked u-backgroundPrimary',
+          disabled && 'is-disabled u-cursorNotAllow u-pointerEventsNone'
+        )}
+        aria-label={ariaLabel}
+        // data-label-on={!nonLabel ? textLabelOn : undefined}
+        // data-label-off={!nonLabel ? textLabelOff : undefined}
+        disabled={Component === 'button' ? disabled : undefined}
       >
-        {checked ? textLabelOn : textLabelOff}
-      </div>
-    )}
-  </div>
-));
+        <div className={classNames('Toggle-handle', 'u-positionAbsolute u-backgroundWhite')} />
+      </Component>
+      {!nonLabel && (
+        <div className={classNames('u-marginLeftExtraSmall', disabled && 'u-textLight')}>
+          {checked ? textLabelOn : textLabelOff}
+        </div>
+      )}
+    </div>
+  )
+);
 Toggle.displayName = 'Toggle';
 Toggle.defaultProps = defaultProps;
 Toggle.propTypes = propTypes;
-export default Toggle;
