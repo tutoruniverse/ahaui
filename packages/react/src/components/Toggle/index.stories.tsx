a2ff8a6a08779775cb821559ebd12e8960865fdd
Kriss Pham <krisspham@gotitapp.co>
2023-03-29 18:52:20 +0700

    react: Update toggle story title

--- packages/react/src/components/Toggle/index.stories.tsx
+++ packages/react/src/components/Toggle/index.stories.tsx
@@ -5,7 +5,7 @@ const ToggleFn: any = Toggle;
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Toggle',
+  title: 'Buttons/Toggle',
   component: ToggleFn,
   argTypes: {
     checked: {
